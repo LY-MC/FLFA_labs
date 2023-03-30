a985096385278d2a31b9363ed71dc7335cd8f6fe
LY-MC <88684853+LY-MC@users.noreply.github.com>
2023-03-30 22:11:29 +0300

    Added lab3

---
+++ lab3/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter input: ");
+        String input = scanner.nextLine();
+
+        Lexer lexer = new Lexer(input);
+        List<Token> tokens = lexer.tokenize();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
---
+++ lab3/src/Token.java
@@ -0,0 +1,7 @@
+public record Token(TokenType identifier, String value) {
+
+    @Override
+    public String toString() {
+        return String.format("Token(%s, %s)", identifier, value);
+    }
+}
