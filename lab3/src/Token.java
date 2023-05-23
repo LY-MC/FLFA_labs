1e52286dd31874b23c8f558729b4cf64197abf0c
Lesenco Maria <leshenko.mri@gmail.com>
2023-05-23 22:03:40 +0300

    delete1

--- lab3/src/Token.java
+++
@@ -1,7 +0,0 @@
-public record Token(TokenType identifier, String value) {
-
-    @Override
-    public String toString() {
-        return String.format("Token(%s, %s)", identifier, value);
-    }
-}
